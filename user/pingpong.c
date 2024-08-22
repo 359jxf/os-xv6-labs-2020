fb2623870638bc6e9980ef93ecd987278cad251b
359jxf <1151976118@qq.com>
2024-08-22 10:35:04 +0800

    lab1

--- user/pingpong.c
+++ user/pingpong.c
@@ -18,12 +18,12 @@ int main() {
 
     if (pid == 0) { // 子进程
         close(p1[1]); // 关闭父进程到子进程的写端
-        close(p2[0]); // 关闭子进程到父进程的读端
 
         // 读取父进程发送的字节
         read(p1[0], buf, 1);
         printf("%d: received ping\n", getpid());
 
+        close(p2[0]); // 关闭子进程到父进程的读端
         // 向父进程发送字节
         write(p2[1], buf, 1);
 
@@ -34,12 +34,15 @@ int main() {
         exit(0);
     } else { // 父进程
         close(p1[0]); // 关闭父进程到子进程的读端
-        close(p2[1]); // 关闭子进程到父进程的写端
 
         // 向子进程发送一个字节
         buf[0] = 'A';
         write(p1[1], buf, 1);
+        
+        // 等待子进程结束
+        wait(0);
 
+        close(p2[1]); // 关闭子进程到父进程的写端
         // 读取子进程返回的字节
         read(p2[0], buf, 1);
         printf("%d: received pong\n", getpid());
@@ -47,9 +50,6 @@ int main() {
         // 关闭剩余的文件描述符
         close(p1[1]);
         close(p2[0]);
-
-        // 等待子进程结束
-        wait(0);
 
         exit(0);
     }
--- user/xargs.c
+++ user/xargs.c
@@ -1,65 +1,54 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/fs.h"
-#include "kernel/fcntl.h"
 #include "kernel/param.h"
 
-#define MAX_BUFFER 512
+// #define MAXARG 1024
 
-int main(int argc, char *argv[]) {
+int
+main(int argc, char *argv[])
+{
+    // 如果参数个数小于 2
     if (argc < 2) {
-        fprintf(2, "Usage: xargs <command> [args...]\n");
+        // 打印参数错误提示
+        fprintf(2, "usage: xargs command\n");
+        // 异常退出
         exit(1);
     }
-
-    char buf[MAX_BUFFER];
-    char *new_argv[MAXARG];
-    int i, n;
-    int pid;
-
-    // Copy original arguments to new_argv
-    for (i = 0; i < argc - 1; i++) {
-        new_argv[i] = argv[i + 1];
+    // 存放子进程 exec 的参数
+    char * argvs[MAXARG];
+    // 索引
+    int index = 0;
+    // 略去 xargs ，用来保存命令行参数
+    for (int i = 1; i < argc; ++i) {
+        argvs[index++] = argv[i];
     }
-
-    while (1) {
-        // Read a line from standard input
-        i = 0;
-        while (1) {
-            n = read(0, &buf[i], 1);
-            if (n < 0) {
-                fprintf(2, "xargs: read error\n");
-                exit(1);
+    // 缓冲区存放从管道读出的数据
+    char buf[MAXARG] = {"\0"};
+    
+    int n;
+	// 0 代表的是管道的 0，也就是从管道循环读取数据
+    while((n = read(0, buf, MAXARG)) > 0 ) {
+        // 临时缓冲区存放追加的参数
+		char temp[MAXARG] = {"\0"};
+        // xargs 命令的参数后面再追加参数
+        argvs[index] = temp;
+        // 内循环获取追加的参数并创建子进程执行命令
+        for(int i = 0; i < strlen(buf); ++i) {
+            // 读取单个输入行，当遇到换行符时，创建子线程
+            if(buf[i] == '\n') {
+                // 创建子线程执行命令
+                if (fork() == 0) {
+                    exec(argv[1], argvs);
+                }
+                // 等待子线程执行完毕
+                wait(0);
+            } else {
+                // 否则，读取管道的输出作为输入
+                temp[i] = buf[i];
             }
-            if (n == 0 || buf[i] == '\n') {
-                break;
-            }
-            i++;
-        }
-        if (n == 0 && i == 0) {
-            // End of input
-            break;
-        }
-        buf[i] = 0; // Null terminate the string
-
-        // Add the line to new_argv
-        new_argv[argc - 1] = buf;
-        new_argv[argc] = 0; // Null terminate the argument list
-
-        // Fork and exec
-        pid = fork();
-        if (pid < 0) {
-            fprintf(2, "xargs: fork failed\n");
-            exit(1);
-        }
-        if (pid == 0) {
-            exec(new_argv[0], new_argv);
-            fprintf(2, "xargs: exec %s failed\n", new_argv[0]);
-            exit(1);
-        } else {
-            wait(0);
         }
     }
+    // 正常退出
     exit(0);
 }
